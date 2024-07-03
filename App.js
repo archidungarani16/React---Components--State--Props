039b87971af1ce4ea3321a7fc62279ea807fa269
archidungarani16 <159746424+archidungarani16@users.noreply.github.com>
2024-07-03 10:57:07 +0530

    add

---
+++ App.js
@@ -0,0 +1,34 @@
+// import logo from './logo.svg';
+import './App.css';
+import './My.css';
+import Test from './Test';
+import Dem from './Dem';
+import Slider from './Slider';
+import Header1 from './Header1';
+import Footer from './Footer';
+import Example from './Example';
+import List from './List';
+import Todolist from './Todolist';
+import Useref from './Useref';
+import Effect from './Effect';
+import Api from './Api';
+import Data from './Data';
+function App() {
+  return (
+    <div className="App color">
+      {/* <Header1/>
+      <Slider />
+      <Footer/> 
+      <Example/>    */}
+      {/* <List /> */}
+      <Todolist/>
+      {/* <Useref/> */}
+      {/* <Effect/> */}
+      {/* <Api/> */}
+      {/* <Data/> */}
+  
+    </div>
+  );
+}
+
+export default App;
